eda64318a49bd3c4eafaf8e99f4d545d103486a8
Madhan-Kumar <gmadhang9@gmail.com>
2019-10-26 13:15:10 +0530

    Adding registration page to project

---
+++ src/main/java/ImageHoster/service/UserService.java
@@ -0,0 +1,12 @@
+package ImageHoster.service;
+
+import ImageHoster.model.User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    public void registerUser(User newUser) {
+        return;
+    }
+}
